7cf0c5f8a185a3f3fc71cf16c4dd006110184f13
jakepro657 <jakepro657@gmail.com>
2024-01-14 01:30:16 +0900

    feat: expanded features

--- src/components/GuidePopUp.tsx
+++ src/components/GuidePopUp.tsx
@@ -4,9 +4,10 @@ type Props = {
   nameHandler: (name: string) => void;
   closeHandler: () => void;
   isOpen: boolean;
+  content: string;
 };
 
-function NamePopUp({ nameHandler, closeHandler, isOpen }: Props) {
+function GuidePopUp({ nameHandler, closeHandler, isOpen, content }: Props) {
   const [name, setName] = useState("");
 
   const onChangeHandler = (e: any) => {
@@ -24,7 +25,8 @@ function NamePopUp({ nameHandler, closeHandler, isOpen }: Props) {
     <>
       <div className="z-50 absolute left-0 top-0 w-full h-full bg-black bg-opacity-20" />
       <div className="z-50 absolute drop-shadow-lg rounded-md top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-80 h-64 bg-white flex flex-col gap-4 justify-center items-center">
-        <p>제 이름은... {"("}성 제외{")"}</p>
+        <p className="text-xs font-bold">*처음 접속시, 새로고침을 해주세요</p>
+        <p>{content}</p>
         <input
           className="w-64 h-10 border-2 text-center border-gray-300 rounded-md outline-none p-2"
           placeholder="(성 제외) 이름을 입력하세요."
@@ -41,4 +43,4 @@ function NamePopUp({ nameHandler, closeHandler, isOpen }: Props) {
   ) : null;
 }
 
-export default NamePopUp;
+export default GuidePopUp;
